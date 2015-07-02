954dcc5f46b906c872af87b2810b7d1d06dd3e21
JMtorii <j.torii4@gmail.com>
2015-07-02 16:25:53 -0400

    More template work

---
+++ Navii-Android/Navii/app/src/main/java/com/teamawesome/navii/activity/debug/DebugLoginActivity.java
@@ -0,0 +1,7 @@
+package com.teamawesome.navii.activity.debug;
+
+/**
+ * Created by JMtorii on 15-07-02.
+ */
+public class DebugLoginActivity {
+}
---
+++ Navii-Android/Navii/app/src/main/java/com/teamawesome/navii/activity/debug/DebugServerActivity.java
@@ -0,0 +1,70 @@
+package com.teamawesome.navii.activity.debug;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.teamawesome.navii.R;
+
+public class DebugServerActivity extends ListActivity {
+
+    String[] titleValues = new String[] { "GET", "POST" };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, titleValues);
+        setListAdapter(adapter);
+        setTitle("Debug Server Activity");
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        // onResume gets called after this to handle the intent
+        setIntent(intent);
+    }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        String item = (String) getListAdapter().getItem(position);
+        Toast.makeText(this, item + " selected", Toast.LENGTH_LONG).show();
+
+        // TODO: make this an enum
+        if (id == 0) {
+//            Intent homeIntent = new Intent(this, MainActivity.class);
+//            startActivity(homeIntent);
+
+        } else {
+
+        }
+    }
+}
