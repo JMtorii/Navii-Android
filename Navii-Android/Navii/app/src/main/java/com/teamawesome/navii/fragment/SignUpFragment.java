4998a32ee9cabc5aeeb5c2794d39ae9438040020
JMtorii <j.torii4@gmail.com>
2015-08-25 19:10:09 -0400

    Getting ready for login and signup

---
+++ Navii-Android/Navii/app/src/main/java/com/teamawesome/navii/fragment/SignUpFragment.java
@@ -0,0 +1,28 @@
+package com.teamawesome.navii.fragment;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.teamawesome.navii.R;
+
+/**
+ * Created by JMtorii on 15-08-25.
+ */
+public class SignUpFragment extends Fragment {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View v = inflater.inflate(R.layout.fragment_signup, container, false);
+        return v;
+    }
+
+}
