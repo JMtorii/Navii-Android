c547ed2fe2ba159e6c7f94e1c0a8e6a376fb53b6
JMtorii <j.torii4@gmail.com>
2015-09-12 18:28:37 -0400

    Sorry. Adding a minor link for future reference

--- Navii-Android/Navii/app/src/main/java/com/teamawesome/navii/util/NaviiPreferenceData.java
+++ Navii-Android/Navii/app/src/main/java/com/teamawesome/navii/util/NaviiPreferenceData.java
@@ -4,5 +4,5 @@ package com.teamawesome.navii.util;
  * Created by JMtorii on 15-09-12.
  */
 public class NaviiPreferenceData {
-    
+//http://stackoverflow.com/questions/11894447/trying-to-maintain-login-state-with-android-sharedpreferences
 }
