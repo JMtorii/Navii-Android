5852556460aa740444e959e4de122439212b08b4
William Kim <williamkim@Williams-MacBook-Pro.local>
2015-09-26 02:08:54 -0400

    Got Preferences to work for BuzzFeed

--- app/src/main/java/com/teamawesome/navii/fragment/BuzzFeedTagsFragment.java
+++ app/src/main/java/com/teamawesome/navii/fragment/BuzzFeedTagsFragment.java
@@ -30,24 +30,6 @@ public class BuzzFeedTagsFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment BuzzFeedTagsFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static BuzzFeedTagsFragment newInstance(String param1, String param2) {
-        BuzzFeedTagsFragment fragment = new BuzzFeedTagsFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     public BuzzFeedTagsFragment() {
         // Required empty public constructor
     }
@@ -55,10 +37,6 @@ public class BuzzFeedTagsFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
 
     @Override
@@ -72,17 +50,6 @@ public class BuzzFeedTagsFragment extends Fragment {
     public void onButtonPressed(Uri uri) {
         if (mListener != null) {
             mListener.onFragmentInteraction(uri);
-        }
-    }
-
-    @Override
-    public void onAttach(Activity activity) {
-        super.onAttach(activity);
-        try {
-            mListener = (OnFragmentInteractionListener) activity;
-        } catch (ClassCastException e) {
-            throw new ClassCastException(activity.toString()
-                    + " must implement OnFragmentInteractionListener");
         }
     }
 
