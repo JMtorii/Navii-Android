469409c9283501b71d12d7ba55297d3219ed1c48
William Kim <williamkim@Williams-MacBook-Pro.local>
2015-11-12 21:53:23 -0500

    Implemented Date Picker Roughly

--- app/src/main/java/com/teamawesome/navii/fragment/planning/ChooseLocationFragment.java
+++ app/src/main/java/com/teamawesome/navii/fragment/planning/ChooseLocationFragment.java
@@ -5,13 +5,24 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.borax12.materialdaterangepicker.date.DatePickerDialog;
+import com.borax12.materialdaterangepicker.time.RadialPickerLayout;
+import com.borax12.materialdaterangepicker.time.TimePickerDialog;
 import com.teamawesome.navii.R;
+
+import java.util.Calendar;
 
 /**
  * Created by JMtorii on 2015-10-13.
  */
-public class ChooseLocationFragment extends Fragment {
+public class ChooseLocationFragment extends Fragment implements
+        DatePickerDialog.OnDateSetListener,TimePickerDialog.OnTimeSetListener {
+
+    private Button mDateButton;
+    private TextView mDateTextView;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -23,7 +34,42 @@ public class ChooseLocationFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_choose_location, container, false);
+        mDateButton = (Button) v.findViewById(R.id.choose_location_date_button);
+        mDateTextView = (TextView) v.findViewById(R.id.choose_location_date_textview);
+
+        // Show a datepicker when the dateButton is clicked
+        mDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar now = Calendar.getInstance();
+                DatePickerDialog dpd = com.borax12.materialdaterangepicker.date.DatePickerDialog.newInstance(
+                        ChooseLocationFragment.this,
+                        now.get(Calendar.YEAR),
+                        now.get(Calendar.MONTH),
+                        now.get(Calendar.DAY_OF_MONTH)
+                );
+                dpd.show(getActivity().getFragmentManager(), "Datepickerdialog");
+            }
+        });
 
         return v;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        DatePickerDialog datePickerDialog = (DatePickerDialog) getActivity().getFragmentManager().findFragmentByTag("Datepickerdialog");
+        if(datePickerDialog != null) datePickerDialog.setOnDateSetListener(this);
+    }
+
+    @Override
+    public void onDateSet(DatePickerDialog view, int year, int monthOfYear, int dayOfMonth,int yearEnd, int monthOfYearEnd, int dayOfMonthEnd) {
+        String date = "You picked the following date: From- "+dayOfMonth+"/"+(++monthOfYear)+"/"+year+" To "+dayOfMonthEnd+"/"+(++monthOfYearEnd)+"/"+yearEnd;
+        mDateTextView.setText(date);
+    }
+
+    @Override
+    public void onTimeSet(RadialPickerLayout radialPickerLayout, int i, int i1, int i2, int i3) {
+
     }
 }
