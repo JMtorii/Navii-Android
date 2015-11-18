04e8c7569cd8b43e32ce7963af94c90e4438b4d4
JMtorii <j.torii4@gmail.com>
2015-11-18 00:42:10 -0500

    Added trending destination view pager

--- app/src/main/java/com/teamawesome/navii/fragment/planning/ChooseLocationFragment.java
+++ app/src/main/java/com/teamawesome/navii/fragment/planning/ChooseLocationFragment.java
@@ -10,21 +10,30 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import com.codetroopers.betterpickers.calendardatepicker.CalendarDatePickerDialogFragment;
+import com.daimajia.slider.library.Animations.DescriptionAnimation;
+import com.daimajia.slider.library.SliderLayout;
+import com.daimajia.slider.library.SliderTypes.BaseSliderView;
+import com.daimajia.slider.library.SliderTypes.TextSliderView;
+import com.daimajia.slider.library.Tricks.ViewPagerEx;
 import com.teamawesome.navii.R;
 
 import org.joda.time.DateTime;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
  * Created by JMtorii on 2015-10-13.
  */
 
-public class ChooseLocationFragment extends Fragment implements CalendarDatePickerDialogFragment.OnDateSetListener {
+public class ChooseLocationFragment extends Fragment implements CalendarDatePickerDialogFragment.OnDateSetListener, BaseSliderView.OnSliderClickListener, ViewPagerEx.OnPageChangeListener {
 
     private static final String FRAG_TAG_DATE_PICKER = "fragment_date_picker_name";
 
     private Button mDateButton;
     private TextView mDateTextView;
+    private SliderLayout mSliderLayout;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -38,6 +47,7 @@ public class ChooseLocationFragment extends Fragment implements CalendarDatePick
         View v = inflater.inflate(R.layout.fragment_choose_location, container, false);
         mDateButton = (Button) v.findViewById(R.id.choose_location_date_button);
         mDateTextView = (TextView) v.findViewById(R.id.choose_location_date_textview);
+        mSliderLayout = (SliderLayout) v.findViewById(R.id.choose_location_slider_layout);
 
         mDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,13 +62,9 @@ public class ChooseLocationFragment extends Fragment implements CalendarDatePick
             }
         });
 
+        setupImageSlider();
 
         return v;
-    }
-
-    @Override
-    public void onDateSet(CalendarDatePickerDialogFragment dialog, int year, int monthOfYear, int dayOfMonth) {
-        mDateTextView.setText("Year: " + year + "\nMonth: " + monthOfYear + "\nDay: " + dayOfMonth);
     }
 
     @Override
@@ -70,5 +76,68 @@ public class ChooseLocationFragment extends Fragment implements CalendarDatePick
         if (calendarDatePickerDialogFragment != null) {
             calendarDatePickerDialogFragment.setOnDateSetListener(this);
         }
+    }
+
+    @Override
+    public void onStop() {
+        mSliderLayout.removeAllSliders();
+        mSliderLayout.stopAutoCycle();
+        super.onStop();
+    }
+
+    @Override
+    public void onDateSet(CalendarDatePickerDialogFragment dialog, int year, int monthOfYear, int dayOfMonth) {
+        mDateTextView.setText("Year: " + year + "\nMonth: " + monthOfYear + "\nDay: " + dayOfMonth);
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+        // nothing to do here
+    }
+
+    @Override
+    public void onPageSelected(int position) {
+        // nothing to do here
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+        // nothing to do here
+    }
+
+    @Override
+    public void onSliderClick(BaseSliderView slider) {
+        // nothing to do here
+    }
+
+    private void setupImageSlider() {
+        Map<String,Integer> file_maps = new HashMap<>();
+        file_maps.put("Toronto Skyline", R.drawable.toronto1);
+        file_maps.put("Paramount Wonderland", R.drawable.toronto2);
+        file_maps.put("Toronto Zoo", R.drawable.toronto3);
+        file_maps.put("Air Canada Centre", R.drawable.toronto4);
+
+        for (String name : file_maps.keySet()) {
+            TextSliderView textSliderView = new TextSliderView(getActivity());
+            // initialize a SliderLayout
+            textSliderView
+                    .description(name)
+                    .image(file_maps.get(name))
+                    .setScaleType(BaseSliderView.ScaleType.Fit)
+                    .setOnSliderClickListener(this);
+
+            //add your extra information
+            textSliderView.bundle(new Bundle());
+            textSliderView.getBundle()
+                    .putString("extra",name);
+
+            mSliderLayout.addSlider(textSliderView);
+        }
+
+        mSliderLayout.setPresetTransformer(SliderLayout.Transformer.Fade);
+        mSliderLayout.setPresetIndicator(SliderLayout.PresetIndicators.Center_Bottom);
+        mSliderLayout.setCustomAnimation(new DescriptionAnimation());
+        mSliderLayout.setDuration(10000);
+        mSliderLayout.addOnPageChangeListener(this);
     }
 }
