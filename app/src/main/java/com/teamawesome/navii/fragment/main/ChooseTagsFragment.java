74093d6940a4d931547d5aa0a3066dec9f823636
jsh0211@gmail.com <jsh0211@gmail.com>
2015-12-03 20:04:25 -0500

    Tags and Preferences

--- app/src/main/java/com/teamawesome/navii/fragment/main/ChooseTagsFragment.java
+++ app/src/main/java/com/teamawesome/navii/fragment/main/ChooseTagsFragment.java
@@ -13,14 +13,20 @@ import com.anton46.collectionitempicker.CollectionPicker;
 import com.anton46.collectionitempicker.Item;
 import com.anton46.collectionitempicker.OnItemClickListener;
 import com.teamawesome.navii.R;
+import com.teamawesome.navii.server.api.UserPreferenceAPI;
+import com.teamawesome.navii.server.model.UserPreference;
+import com.teamawesome.navii.util.Constants;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import retrofit.JacksonConverterFactory;
+import retrofit.Retrofit;
+
 /**
  * Created by sjung on 18/11/15.
  */
-public class ChooseTagsFragment extends Fragment {
+public class ChooseTagsFragment extends MainFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -58,6 +64,13 @@ public class ChooseTagsFragment extends Fragment {
                 new Item("artificial", "artificial"),
                 new Item("artistic", "artistic")};
 
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(Constants.SERVER_URL)    // THIS ONLY WORKS ON JUN'S CASE
+                .addConverterFactory(JacksonConverterFactory.create())
+                .build();
+
+        UserPreferenceAPI userPreferenceAPI = retrofit.create(UserPreferenceAPI.class);
+
         List<Item> tags = new ArrayList<>();
 
         for (Item item : items) {
@@ -77,7 +90,11 @@ public class ChooseTagsFragment extends Fragment {
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Log.v("ChooseTagsFragment", "onClick()");
 
+                parentActivity.switchFragment(new ItineraryRecommendFragment(), Constants
+                        .NO_ANIM, Constants.NO_ANIM, Constants.ITINERARY_RECOMMEND_FRAGMENT,
+                        true, true, true);
             }
         });
         return view;
---
+++ app/src/main/java/com/teamawesome/navii/server/api/TagsAPI.java
@@ -0,0 +1,7 @@
+package com.teamawesome.navii.server.api;
+
+/**
+ * Created by sjung on 03/12/15.
+ */
+public interface TagsAPI {
+}
