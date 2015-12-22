e7fe0d9c1b7e7b4f87a8976210e06bcbbc9b67fe
JMtorii <j.torii4@gmail.com>
2015-12-21 19:34:06 -0500

    Enabled multidexing

--- app/src/main/java/com/teamawesome/navii/NaviiApplication.java
+++ app/src/main/java/com/teamawesome/navii/NaviiApplication.java
@@ -2,6 +2,7 @@ package com.teamawesome.navii;
 
 import android.app.Application;
 import android.content.Context;
+import android.support.multidex.MultiDex;
 
 /**
  * Created by JMtorii on 2015-11-21.
@@ -9,9 +10,16 @@ import android.content.Context;
 public class NaviiApplication extends Application {
     private static Context context;
 
+    @Override
     public void onCreate() {
         super.onCreate();
         NaviiApplication.context = getApplicationContext();
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+        MultiDex.install(this);
     }
 
     public static Context getAppContext() {
