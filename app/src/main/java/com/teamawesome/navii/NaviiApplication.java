f1d581798e9458be33dbc69f923befb647e349af
JMtorii <j.torii4@gmail.com>
2015-12-22 18:51:06 -0500

    Revert this whole fucking bs

--- app/src/main/java/com/teamawesome/navii/NaviiApplication.java
+++ app/src/main/java/com/teamawesome/navii/NaviiApplication.java
@@ -2,7 +2,6 @@ package com.teamawesome.navii;
 
 import android.app.Application;
 import android.content.Context;
-import android.support.multidex.MultiDex;
 
 /**
  * Created by JMtorii on 2015-11-21.
@@ -14,12 +13,6 @@ public class NaviiApplication extends Application {
     public void onCreate() {
         super.onCreate();
         NaviiApplication.context = getApplicationContext();
-    }
-
-    @Override
-    protected void attachBaseContext(Context base) {
-        super.attachBaseContext(base);
-        MultiDex.install(this);
     }
 
     public static Context getAppContext() {
