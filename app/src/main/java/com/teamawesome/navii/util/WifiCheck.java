4f78c5e83ff026825551f68160789b5df93de325
iantel <ian.dn.tran@gmail.com>
2016-04-08 23:34:08 -0400

    Added detection for internet connection

---
+++ app/src/main/java/com/teamawesome/navii/util/WifiCheck.java
@@ -0,0 +1,26 @@
+package com.teamawesome.navii.util;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+import com.teamawesome.navii.fragment.main.NaviiFragment;
+
+/**
+ * Created by Ian on 3/29/2016.
+ * Checks wifi connection mid use.
+ */
+public class WifiCheck {
+
+    /**
+     *
+     * @param f the fragment which the user is viewing
+     * @return True iff the user is connected to wifi
+     */
+    public static boolean isConnected(NaviiFragment f){
+        ConnectivityManager connectivityManager =
+                (ConnectivityManager) f.getActivity().getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo wifiInfo = connectivityManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        return wifiInfo.isConnected();
+    }
+}
