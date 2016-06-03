d4f967e1d5ca5aa8f0163bcfec7ee8a4c7565c5e
JMtorii <j.torii4@gmail.com>
2016-06-03 00:20:25 -0400

    Started custom main button

---
+++ app/src/main/java/com/teamawesome/navii/views/LatoType.java
@@ -0,0 +1,9 @@
+package com.teamawesome.navii.views;
+
+/**
+ * Created by JMtorii on 16-06-02.
+ */
+public interface LatoType {
+    String[] fontTypes = {"Regular", "Black", "BlackItalic", "Bold", "BoldItalic", "Hairline", "HairlineItalic", "Heavy", "HeavyItalic", "Italic", "Light", "LightItalic", "Medium", "MediumItalic", "Semibold", "SemiboldItalic", "Thin", "ThinItalic"};
+    String defaultType = fontTypes[0];
+}
---
+++ app/src/main/java/com/teamawesome/navii/views/MainLatoButton.java
@@ -0,0 +1,65 @@
+package com.teamawesome.navii.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.Button;
+
+import com.teamawesome.navii.R;
+
+import java.util.Arrays;
+
+/**
+ * Created by JMtorii on 16-06-02.
+ */
+public class MainLatoButton extends Button implements LatoType {
+    private static final String BASE_FONT_NAME = "Lato-";
+    private Context mContext;
+
+    public MainLatoButton(Context context) {
+        super(context);
+        mContext = context;
+
+        init(null);
+    }
+
+    public MainLatoButton(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public MainLatoButton(Context context, AttributeSet attrs, int defStyleAttrs) {
+        super(context, attrs, defStyleAttrs);
+        init(attrs);
+    }
+
+    private void init(AttributeSet attrs) {
+        if (attrs != null) {
+            TypedArray typedArray = getContext().obtainStyledAttributes(attrs, R.styleable.MainLatoButton);
+            String fontType = typedArray.getString(R.styleable.MainLatoButton_fontType);
+            setFont(fontType);
+            typedArray.recycle();
+        }
+
+        setBackgroundResource(R.drawable.main_lato_button_bg);
+    }
+
+    public void setFont(String fontType) {
+        String fontName = BASE_FONT_NAME;
+        if (fontType == null) {
+            fontName += defaultType;
+        } else if (Arrays.asList(fontTypes).contains(fontType)) {
+            fontName += fontType;
+        } else {
+            fontName += defaultType;
+        }
+
+        setTypeface(fontName);
+    }
+
+    public void setTypeface(String fontName) {
+        Typeface typeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/" + fontName + ".ttf");
+        setTypeface(typeface);
+    }
+}
