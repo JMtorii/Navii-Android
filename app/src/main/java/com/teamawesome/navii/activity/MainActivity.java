0787d41c4576c21854f093545c3591f5505ce28f
JMtorii <j.torii4@gmail.com>
2016-06-04 15:58:37 -0400

    Imagine. A cool parallax background. Yeah. It happened

--- app/src/main/java/com/teamawesome/navii/activity/MainActivity.java
+++ app/src/main/java/com/teamawesome/navii/activity/MainActivity.java
@@ -65,7 +65,6 @@ public class MainActivity extends NaviiActivity implements NavigationView.OnNavi
         setupActionBar();
         setupNavigationView();
         setupParallaxViews();
-
     }
 
     @Override
