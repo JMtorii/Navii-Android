781f8c6eb6c541d92ce8a87ddde1a06ae3676a1b
William Kim <scarf3blue@gmail.com>
2016-06-04 17:29:00 -0400

    Do shit for TravelDurationFragment

--- app/src/main/java/com/teamawesome/navii/fragment/TravelDurationFragment.java
+++ app/src/main/java/com/teamawesome/navii/fragment/TravelDurationFragment.java
@@ -1,26 +1,79 @@
 package com.teamawesome.navii.fragment;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.DatePicker;
 import android.widget.TextView;
 
 import com.teamawesome.navii.R;
+import com.teamawesome.navii.activity.MainActivity;
+import com.teamawesome.navii.views.MainLatoEditText;
+import com.teamawesome.navii.views.MainLatoTextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by jtorii on 16-05-17.
  */
 public class TravelDurationFragment extends Fragment {
 
-    private TextView mPartTwoTextView;
+    @BindView(R.id.duration_from_text_view)
+    MainLatoTextView mFromTextView;
+
+    @BindView(R.id.duration_to_text_view)
+    MainLatoTextView mToTextView;
+
+    @BindView(R.id.duration_from_date)
+    MainLatoEditText mFromEditText;
+
+    @BindView(R.id.duration_to_date)
+    MainLatoEditText mToEditText;
+
+    private Calendar myCalendar = Calendar.getInstance();
+    private DatePickerDialog.OnDateSetListener date;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View v = inflater.inflate(R.layout.fragment_travel_duration, container, false);
-        mPartTwoTextView = (TextView) v.findViewById(R.id.part_two_text_view);
+
+        ButterKnife.bind(this, v);
+
+        date = new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int year, int monthOfYear,
+                                  int dayOfMonth) {
+                myCalendar.set(Calendar.YEAR, year);
+                myCalendar.set(Calendar.MONTH, monthOfYear);
+                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                updateLabel();
+            }
+        };
+
+        mFromEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new DatePickerDialog(getActivity(), date, myCalendar
+                        .get(Calendar.YEAR), myCalendar.get(Calendar.MONTH),
+                        myCalendar.get(Calendar.DAY_OF_MONTH)).show();
+            }
+        });
 
         return v;
+    }
+
+    private void updateLabel() {
+        String myFormat = "MM/dd/yy";
+        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+        mFromEditText.setText(sdf.format(myCalendar.getTime()));
     }
 }
