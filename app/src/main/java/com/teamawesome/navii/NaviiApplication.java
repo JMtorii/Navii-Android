3fd10b3821d163f88eb6766a4a5976152b4a970d
Jun Mark Torii <j.torii4@gmail.com>
2016-06-11 15:08:47 -0400

    Setting up for an event bus to use in all activities

--- app/src/main/java/com/teamawesome/navii/NaviiApplication.java
+++ app/src/main/java/com/teamawesome/navii/NaviiApplication.java
@@ -3,19 +3,35 @@ package com.teamawesome.navii;
 import android.app.Application;
 import android.content.Context;
 
+import com.teamawesome.navii.util.RxBus;
+
 /**
  * Created by JMtorii on 2015-11-21.
  */
 public class NaviiApplication extends Application {
+    private static NaviiApplication sInstance;
     private static Context context;
+    private static RxBus bus;
+
+    public static NaviiApplication getInstance() {
+        return sInstance;
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        NaviiApplication.context = getApplicationContext();
+        sInstance = this;
+        context = getApplicationContext();
     }
 
-    public static Context getAppContext() {
-        return NaviiApplication.context;
+    public Context getAppContext() {
+        return context;
+    }
+
+    public RxBus getBus() {
+        if (bus == null) {
+            bus = new RxBus();
+        }
+        return bus;
     }
 }
