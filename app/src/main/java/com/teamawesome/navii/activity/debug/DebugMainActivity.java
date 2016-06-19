c0d04b12ce9429df65393984860db37829844bee
enc <evan.crothers@gmail.com>
2016-06-19 13:30:54 -0400

    Stub for image upload test activity.

--- app/src/main/java/com/teamawesome/navii/activity/debug/DebugMainActivity.java
+++ app/src/main/java/com/teamawesome/navii/activity/debug/DebugMainActivity.java
@@ -11,6 +11,7 @@ import android.widget.ListView;
 
 import com.teamawesome.navii.R;
 import com.teamawesome.navii.activity.HeartAndSoulDetailsActivity;
+import com.teamawesome.navii.activity.ImageUploadTestActivity;
 import com.teamawesome.navii.activity.MainActivity;
 import com.teamawesome.navii.activity.PackageOverviewActivity;
 import com.teamawesome.navii.server.api.UserAPI;
@@ -105,6 +106,9 @@ public class DebugMainActivity extends ListActivity {
         } else if (id == 4) {       // Heart and Soul Details Activity
             Intent heartAndSoulDetailsActivity = new Intent(this, HeartAndSoulDetailsActivity.class);
             startActivity(heartAndSoulDetailsActivity);
+        } else if (id == 5) {       // Upload profile image test
+            Intent imageUploadTestActivity = new Intent(this, ImageUploadTestActivity.class);
+            startActivity(imageUploadTestActivity);
         }
     }
 }
