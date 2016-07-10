f91431b33be21453a4ce82b5bb749e2d7f10da85
William Kim <scarf3blue@gmail.com>
2016-07-10 13:42:52 -0400

    Add validation to from-to page

--- app/src/main/java/com/teamawesome/navii/fragment/main/TravelDurationFragment.java
+++ app/src/main/java/com/teamawesome/navii/fragment/main/TravelDurationFragment.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.DatePicker;
+import android.widget.Toast;
 
 import com.teamawesome.navii.R;
 import com.teamawesome.navii.views.MainLatoTextView;
@@ -50,10 +51,21 @@ public class TravelDurationFragment extends NaviiParallaxFragment {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear,
                                   int dayOfMonth) {
-                myCalendar.set(Calendar.YEAR, year);
-                myCalendar.set(Calendar.MONTH, monthOfYear);
-                myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                updateLabel();
+                Calendar suggestedCalendar = Calendar.getInstance();
+                suggestedCalendar.set(Calendar.YEAR, year);
+                suggestedCalendar.set(Calendar.MONTH, monthOfYear);
+                suggestedCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                if (Calendar.getInstance().compareTo(suggestedCalendar) <= 0) {
+                    myCalendar.set(Calendar.YEAR, year);
+                    myCalendar.set(Calendar.MONTH, monthOfYear);
+                    myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                    updateLabel();
+                } else {
+                    Toast.makeText(getActivity(),
+                            "The starting date cannot be set before today.",
+                            Toast.LENGTH_LONG).show();
+                }
             }
         };
 
@@ -61,10 +73,21 @@ public class TravelDurationFragment extends NaviiParallaxFragment {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear,
                                   int dayOfMonth) {
-                myCalendar2.set(Calendar.YEAR, year);
-                myCalendar2.set(Calendar.MONTH, monthOfYear);
-                myCalendar2.set(Calendar.DAY_OF_MONTH, dayOfMonth);
-                updateLabel2();
+                Calendar suggestedCalendar = Calendar.getInstance();
+                suggestedCalendar.set(Calendar.YEAR, year);
+                suggestedCalendar.set(Calendar.MONTH, monthOfYear);
+                suggestedCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+
+                if (myCalendar.compareTo(suggestedCalendar) < 0) {
+                    myCalendar2.set(Calendar.YEAR, year);
+                    myCalendar2.set(Calendar.MONTH, monthOfYear);
+                    myCalendar2.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+                    updateLabel2();
+                } else {
+                    Toast.makeText(getActivity(),
+                            "The ending date must come after the starting date.",
+                            Toast.LENGTH_LONG).show();
+                }
             }
         };
 
@@ -85,6 +108,11 @@ public class TravelDurationFragment extends NaviiParallaxFragment {
                         myCalendar2.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
+
+        myCalendar2.add(Calendar.DAY_OF_YEAR, 1);
+
+        updateLabel();
+        updateLabel2();
 
         return v;
     }
