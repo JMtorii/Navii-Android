7cccfc2f4a6650dca6b42069eda4f131e8726c7f
Jun Mark Torii <j.torii4@gmail.com>
2016-07-12 00:49:06 -0400

    Updated toolbar in the Package Overview activity

--- app/src/main/java/com/teamawesome/navii/activity/PackageOverviewActivity.java
+++ app/src/main/java/com/teamawesome/navii/activity/PackageOverviewActivity.java
@@ -3,12 +3,12 @@ package com.teamawesome.navii.activity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
-import android.util.Log;
 
 import com.teamawesome.navii.R;
 import com.teamawesome.navii.adapter.PackageOverviewRecyclerViewAdapter;
 import com.teamawesome.navii.server.model.Attraction;
 import com.teamawesome.navii.util.Constants;
+import com.teamawesome.navii.util.ToolbarConfiguration;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,18 +19,31 @@ import butterknife.ButterKnife;
 /**
  * Created by JMtorii on 16-06-04.
  */
-public class PackageOverviewActivity extends NaviiActivity {
+public class PackageOverviewActivity extends NaviiToolbarActivity {
     @BindView(R.id.package_overview_recycler_view)
     RecyclerView recyclerView;
 
     @Override
+    public ToolbarConfiguration getToolbarConfiguration() {
+        return ToolbarConfiguration.PackageOverview;
+    }
+
+    @Override
+    public void onLeftButtonClick() {
+        super.onBackPressed();
+    }
+
+    @Override
+    public void onRightButtonClick() {
+        // nothing to do here
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_package_overview);
         ButterKnife.bind(this);
 
         List<Attraction> attractionList = getIntent().getParcelableArrayListExtra(Constants.INTENT_ATTRACTION_LIST);
-
         List<String> photoUriList = new ArrayList<>();
 
         for (Attraction attraction : attractionList) {
