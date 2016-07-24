6b90130036944675628c7c08ce0a13ee7691b716
Jun Mark Torii <j.torii4@gmail.com>
2016-07-24 13:27:15 -0400

    Started implementing onboarding activity

--- app/src/main/java/com/teamawesome/navii/activity/debug/DebugMainActivity.java
+++ app/src/main/java/com/teamawesome/navii/activity/debug/DebugMainActivity.java
@@ -3,7 +3,6 @@
  import android.app.ListActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.ArrayAdapter;
@@ -13,20 +12,12 @@ import com.teamawesome.navii.NaviiApplication;
 import com.teamawesome.navii.R;
 import com.teamawesome.navii.activity.ItineraryScheduleActivity;
 import com.teamawesome.navii.activity.MainActivity;
-import com.teamawesome.navii.activity.PreferencesActivity;
+ import com.teamawesome.navii.activity.OnboardingActivity;
+ import com.teamawesome.navii.activity.PreferencesActivity;
 import com.teamawesome.navii.activity.UploadImageTestActivity;
-import com.teamawesome.navii.server.api.UserAPI;
 import com.teamawesome.navii.server.model.Itinerary;
-import com.teamawesome.navii.server.model.User;
 import com.teamawesome.navii.util.Constants;
-import com.teamawesome.navii.util.NaviiMath;
 import com.teamawesome.navii.util.NaviiPreferenceData;
-
-import retrofit.Call;
-import retrofit.Callback;
-import retrofit.JacksonConverterFactory;
-import retrofit.Response;
-import retrofit.Retrofit;
 
 // TODO: change this to AppCompatActivity for more flexibility
 public class DebugMainActivity extends ListActivity {
@@ -65,54 +56,23 @@ public class DebugMainActivity extends ListActivity {
     protected void onListItemClick(ListView l, View v, int position, long id) {
         String item = (String) getListAdapter().getItem(position);
 
-        // TODO: make this an enum
         if (id == 0) {              // Actual application
             Intent mainIntent = new Intent(this, MainActivity.class);
             startActivity(mainIntent);
-        } else if (id == 1) {       // Create user
-            Retrofit retrofit = new Retrofit.Builder()
-                    .baseUrl(Constants.SERVER_URL)    // THIS ONLY WORKS IN JUN'S CASE
-                    .addConverterFactory(JacksonConverterFactory.create())
-                    .build();
-
-            UserAPI userAPI = retrofit.create(UserAPI.class);
-
-            int randomUniformInt = NaviiMath.randInt(0, 1000);
-            User user = new User.Builder()
-                    .username("android-user" + String.valueOf(randomUniformInt))
-                    .password("android-password" + String.valueOf(randomUniformInt))
-                    .salt("android-salt")
-                    .build();
-            Call<Void> call = userAPI.createUser(user);
-
-            // This does an async call.
-            // Use "execute" instead for a sync call.
-            // Call "call.cancel()" to cancel a running request.
-            call.enqueue(new Callback<Void>() {
-                @Override
-                public void onResponse(Response<Void> response, Retrofit retrofit) {
-                    Log.i("response: code", String.valueOf(response.code()));
-                    Log.i("response: value", String.valueOf(response.body()));
-                }
-
-                @Override
-                public void onFailure(Throwable t) {
-                    Log.i("failed", t.getMessage());
-                }
-            });
-        } else if (id == 2) {       // Set ip address
-            NaviiPreferenceData.setIPAddress(Constants.SERVER_URL_JUN);
-        } else if (id == 3) {       // Upload profile image test
+        } else if (id == 1) {       // Upload profile image test
             Intent imageUploadTestActivity = new Intent(this, UploadImageTestActivity.class);
             startActivity(imageUploadTestActivity);
-        } else if (id == 4) {       // Heart and Soul Details Activity
+        } else if (id == 2) {       // Heart and Soul Details Activity
             Intent preferenceActivity = new Intent(this, PreferencesActivity.class);
             startActivity(preferenceActivity);
-        } else if (id == 5) {       // Itinerary Schedule
+        } else if (id == 3) {       // Itinerary Schedule
             Intent itineraryScheduleActivity = new Intent(this, ItineraryScheduleActivity.class);
             Itinerary itinerary = new Itinerary();
             NaviiApplication.getInstance().getBus().send(itinerary);
             startActivity(itineraryScheduleActivity);
+        } else if (id == 4) {       // Onboarding
+            Intent onboardingActivity = new Intent(this, OnboardingActivity.class);
+            startActivity(onboardingActivity);
         }
     }
 }
