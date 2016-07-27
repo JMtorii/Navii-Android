f6b4fd163723647f1114211676cb1f641f402893
Ian <ian.dn.tran@gmail.com>
2016-07-27 15:24:17 -0500

    Fixed swiping on main activity not working for Android 6.0

--- app/src/main/java/com/teamawesome/navii/views/ParallaxViewPager.java
+++ app/src/main/java/com/teamawesome/navii/views/ParallaxViewPager.java
@@ -62,46 +62,60 @@ public class ParallaxViewPager extends ViewPager {
     public void configure(ParallaxHorizontalScrollView parallaxHorizontalScrollView, MainActivity mainActivity) {
         this.mActivity = mainActivity;
         this.mParallaxHorizontalScrollView = parallaxHorizontalScrollView;
-        overrideScrollListener();
+        //overrideScrollListener();
     }
 
     public void setParallaxHorizontalScrollViewPosition(int scrollX, int position) {
         this.mParallaxHorizontalScrollView.scrollTo(Math.round((scrollX * mParallaxVelocity) + (position * mParallaxVelocity * this.mParallaxHorizontalScrollView.getWidth())), 0);
     }
 
-    private void overrideScrollListener() {
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            this.setOnScrollChangeListener(new OnScrollChangeListener() {
-                @Override
-                public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
-                    setParallaxHorizontalScrollViewPosition(scrollX, 0);
-                }
-            });
-        } else {
-
-            this.addOnPageChangeListener(new OnPageChangeListener() {
-                @Override
-                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-                    Log.d("position: ", String.valueOf(position));
-                    if (position == 3) {
-                        mActivity.getmNextButton().setVisibility(INVISIBLE);
-                    }
-                    else {
-                        mActivity.getmNextButton().setVisibility(VISIBLE);
-                    }
-                    setParallaxHorizontalScrollViewPosition(positionOffsetPixels, position);
-                }
-
-                @Override
-                public void onPageSelected(int position) {
-                    // nothing to do
-                }
-
-                @Override
-                public void onPageScrollStateChanged(int state) {
-                    // nothing to do
-                }
-            });
+    @Override
+    protected void onPageScrolled(int position, float offset, int offsetPixels) {
+        super.onPageScrolled(position, offset, offsetPixels);
+        if (position == 3) {
+            mActivity.getmNextButton().setVisibility(INVISIBLE);
         }
+        else {
+            mActivity.getmNextButton().setVisibility(VISIBLE);
+        }
+        setParallaxHorizontalScrollViewPosition(offsetPixels, position);
     }
+    //Not deleting this in case above function messes up
+
+//    private void overrideScrollListener() {
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+//            this.setOnScrollChangeListener(new OnScrollChangeListener() {
+//                @Override
+//                public void onScrollChange(View v, int scrollX, int scrollY, int oldScrollX, int oldScrollY) {
+//                    Log.d("Scroll X:", String.valueOf(scrollX));
+//                    setParallaxHorizontalScrollViewPosition(scrollX, 0);
+//                }
+//            });
+//        } else {
+//
+//            this.addOnPageChangeListener(new OnPageChangeListener() {
+//                @Override
+//                public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//                    Log.d("position: ", String.valueOf(position));
+//                    if (position == 3) {
+//                        mActivity.getmNextButton().setVisibility(INVISIBLE);
+//                    }
+//                    else {
+//                        mActivity.getmNextButton().setVisibility(VISIBLE);
+//                    }
+//                    setParallaxHorizontalScrollViewPosition(positionOffsetPixels, position);
+//                }
+//
+//                @Override
+//                public void onPageSelected(int position) {
+//                    // nothing to do
+//                }
+//
+//                @Override
+//                public void onPageScrollStateChanged(int state) {
+//                    // nothing to do
+//                }
+//            });
+//        }
+//    }
 }
