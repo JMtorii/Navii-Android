1a0804263c3be9c1518aa1d444e81f3f26b6db5f
William Kim <scarf3blue@gmail.com>
2016-08-06 18:20:05 -0400

    Add ScrollView to a bunch of shit

--- app/src/main/java/com/teamawesome/navii/activity/OnboardingActivity.java
+++ app/src/main/java/com/teamawesome/navii/activity/OnboardingActivity.java
@@ -1,10 +1,8 @@
 package com.teamawesome.navii.activity;
 
-import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
 
 import com.teamawesome.navii.R;
 import com.teamawesome.navii.adapter.OnboardingPagerAdapter;
@@ -30,8 +28,8 @@ public class OnboardingActivity extends AppCompatActivity {
         setContentView(R.layout.activity_onboarding);
         ButterKnife.bind(this);
 
-        getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
-        getWindow().setStatusBarColor(Color.TRANSPARENT);
+        //getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
+        //getWindow().setStatusBarColor(Color.TRANSPARENT);
 
         OnboardingPagerAdapter mPageAdapter = new OnboardingPagerAdapter(this.getSupportFragmentManager(), this);
         mViewPager.setAdapter(mPageAdapter);
